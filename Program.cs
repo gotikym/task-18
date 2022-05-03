8e9a33eef5bc5ab10f884fef54c9126dcc4d7d65
One <One@DESKTOP-MIB22JC>
2022-05-03 18:37:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        byte minmalHpBoss = 0;
+        int maxmumHpBoss = 1010;
+        int hpBoss = random.Next(minmalHpBoss, maxmumHpBoss);
+        byte damageForOneHit = 2;
+        int powerOfDamage = 0;
+        while (hpBoss > Math.Pow(damageForOneHit, powerOfDamage))
+        {
+            powerOfDamage++;
+        }
+        Console.WriteLine($"При уроне в {damageForOneHit} единицы от рукопашного боя - для сноса хлеборезки босса с {hpBoss} хп, " +
+            $" достаточно {damageForOneHit}^{powerOfDamage} ударов кулаком");
+    }
+}
